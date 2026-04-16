135ecba9e6622e7463fbb4d6b3bd3e511a24338b
Qahraman H Taha <162983685+qahramantaha@users.noreply.github.com>
2026-04-16 17:49:14 +0000

    fix: installed location package and url fix

--- src/screens/BookmarksScreen.js
+++ src/screens/BookmarksScreen.js
@@ -11,9 +11,9 @@ const getApiUrl = () => {
   const debuggerHost = Constants.expoConfig?.hostUri || Constants.manifest?.debuggerHost;
   if (debuggerHost) {
     const ip = debuggerHost.split(':')[0];
-    return `http://${ip}:5000/api`;
+    return `http://${ip}:3000/api`;
   }
-  return 'http://localhost:5000/api';
+  return 'http://localhost:3000/api';
 };
 
 const API_URL = getApiUrl();
